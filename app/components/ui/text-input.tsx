58fc56027b9d670fe6e9d4f067707aad848b8f94
Igor Henrique <igorhenrique102@gmail.com>
2025-09-21 20:33:34 -0300

    feat: criar o componente de text-input

---
+++ app/components/ui/text-input.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+export function TextInput({ ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
+    return (
+        <input {...props} className={cn(`
+            w-full
+            p-3
+            bg-background-secondary
+            text-content-heading
+            placeholder:text-content-placeholder
+            rounded-xl
+            border border-transparent
+            hover:border-border-secondary
+            hover:text-content-body
+            active:border-border-tertiary`,
+            props.className
+        )} />
+    )
+}
