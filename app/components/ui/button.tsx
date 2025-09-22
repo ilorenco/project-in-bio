166f1a7a8f3765310bf3847b009943ce5ba1e7dc
Igor Henrique <igorhenrique102@gmail.com>
2025-09-21 21:06:50 -0300

    fix: adicionar a propriedade rounded-xl ao componente button

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 export function Button({ 
     children, variant = "primary", ...props }: { children: React.ReactNode, variant?: "primary" | "secondary" | "ghost" } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
     return (
-        <button {...props} className={cn("p-3 text-content-heading font-bold white-space-nowrap hover:opacity-95 disabled:opacity-70",
+        <button {...props} className={cn("p-3 text-content-heading rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70",
             variant === "primary" && "bg-accent-purple",
             variant === "secondary" && "bg-background-tertiary",
             variant === "ghost" && "border-border-primary",
