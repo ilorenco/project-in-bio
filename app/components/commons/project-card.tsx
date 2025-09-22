eecd350114214a7480829a730cd73e10e530ae17
Igor Henrique <igorhenrique102@gmail.com>
2025-09-22 00:54:20 -0300

    feat: criar o componente project-card

---
+++ app/components/commons/project-card.tsx
@@ -0,0 +1,27 @@
+export function ProjectCard() {
+    return (
+        <div className="w-[430px] h-[132px] flex gap-5 bg-background-secondary p-3 rounded-[20px] border border-transparent hover:border-border-secondary">
+            <div className="size-24 rounded-md overflow-hidden flex-shrink-0">
+                <img 
+                src="/project1.jpg" 
+                className="w-full h-full object-cover"
+                />
+            </div>
+            <div className="flex flex-col gap-2">
+                <span className="uppercase text-xs font-bold text-accent-green">
+                    10 cliques
+                </span>
+
+                <div className="flex flex-col">
+                    <span className="text-white font-bold text-xl">
+                        Projeto 1
+                    </span>
+                    <span className="text-content-body text-sm">
+                        Descrição do projeto
+                    </span>
+                </div>
+
+            </div>
+        </div>
+    )
+}
