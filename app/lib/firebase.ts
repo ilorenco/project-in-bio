d0ee6bdc874b41d850ca7f4d04681a9bf7cd6455
Igor Henrique <igorhenrique102@gmail.com>
2025-09-27 01:26:59 -0300

    fix: corrigir o acesso a variável de ambiente

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 import "server-only";
 
-const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY!, "base64").toString("utf-8");
+const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64!, "base64").toString("utf-8");
 
 export const firebaseCert = cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
