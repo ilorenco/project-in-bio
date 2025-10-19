03a36ce6c0e12b3f75ce984362b17854b92e774a
Igor Henrique <igorhenrique102@gmail.com>
2025-10-18 21:19:59 -0300

    feat: configurar webhooks da stripe

--- app/api/stripe/create-checkout/route.ts
+++ app/api/stripe/create-checkout/route.ts
@@ -1,3 +1,5 @@
+import { auth } from "@/lib/auth"
+import { db } from "@/lib/firebase"
 import stripe from "@/lib/stripe"
 import { NextResponse } from "next/server"
 
@@ -6,17 +8,52 @@ export async function POST(request: Request) {
 
     const price = isSubscription ? process.env.STRIPE_SUBSCRIPTION_PRICE_ID : process.env.STRIPE_PRICE_ID
 
+    const userSession = await auth()
+
+    if (!userSession || !userSession.user?.id || !userSession.user?.email) {
+        return new NextResponse(null, { status: 401 })
+    }
+
+    const userId = userSession.user?.id
+    const userEmail = userSession.user?.email
+    const userName = userSession.user?.name
+
+    const userRef = db.collection("users").doc(userId || "")
+    const userDoc = await userRef.get()
+
+    let customerId
+
+    if (userDoc.exists) {
+        customerId = userDoc.data()?.customerId
+
+    }
+    
+    if (!customerId) {
+        const newCustomer = await stripe.customers.create({
+            email: userEmail,
+            name: userName || "Sem nome",
+            metadata: {
+                userId: userId
+            }
+        })
+
+        customerId = newCustomer.id
+
+        await userRef.update({ customerId })
+    }
+
     const session = await stripe.checkout.sessions.create({
-        // customer :,
+        customer: customerId,
         line_items: [{
             price: price,
             quantity: 1,
         }],
         mode: isSubscription ? "subscription" : "payment",
-        metadata,
         payment_method_types: isSubscription ? ["card"] : ["card", "boleto"],
         success_url: `${request.headers.get("origin")}/${metadata.profileId}`,
         cancel_url: `${request.headers.get("origin")}/${metadata.profileId}/upgrade`,
+        client_reference_id: userId,
+        metadata
     })
 
     return NextResponse.json({
